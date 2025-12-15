037ac2e0c50f26063a529d1d9a9955d3d432e637
aaronBIOO <aaronnabioo@gmail.com>
2025-12-15 09:52:15 +0000

    fix: path fixes

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,19 @@ module.exports = function (api) {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }], 
       'nativewind/babel'
-    ],  
+    ], 
+    plugins: [
+      // 3. Module Resolver (REQUIRED for your "@/..." imports to work)
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src', // Maps "@/components/..." to "./src/components/..."
+          },
+        },
+      ],
+      // (Optional) If you installed Reanimated for animations, its plugin goes here at the end:
+      // 'react-native-reanimated/plugin',
+    ], 
   };
 };
