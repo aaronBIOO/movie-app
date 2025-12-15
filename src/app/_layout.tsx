8d5ec4ecf542f6dd590615ca4f30eeb0624ea0ca
aaronBIOO <aaronnabioo@gmail.com>
2025-12-15 12:29:22 +0000

    app layout and dynamic movie page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,21 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import "./globals.css";
 
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)" 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="movie/[id]" 
+          options={{ headerShown: false }} 
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
