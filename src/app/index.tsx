8327ac0aac123bb7d697521af2c67fb328b05aee
aaronBIOO <aaronnabioo@gmail.com>
2025-12-15 11:21:05 +0000

    expo routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,17 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold text-accent">
+      <Text className="text-2xl font-bold text-accent mb-4">
         Welcome here!
       </Text>
+
+      <View className="flex flex-col gap-2 text-center justify-center items-center">
+        <Link href="/onboarding">onboarding</Link>
+        <Link href={{ pathname: "/movie/[id]", params: { id: "avengers" } }}>Avengers movie</Link>
+      </View>
     </View>
   );
 }
