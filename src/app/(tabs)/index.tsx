9d18491885008941669d0ad5953cbe96fcf4859e
aaronBIOO <aaronnabioo@gmail.com>
2025-12-15 12:28:48 +0000

    key tabs

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-accent mb-4">
+        Welcome here!
+      </Text>
+
+      
+    </View>
+  );
+}
